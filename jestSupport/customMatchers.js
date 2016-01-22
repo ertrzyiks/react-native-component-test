0dc1c9deb6ac0451154be89a16e724fe03245643
Mateusz Derks <mateusz.derks@schibsted.pl>
2016-01-22 11:36:41 +0100

    Add wrecker.yml

--- jestSupport/customMatchers.js
+++ jestSupport/customMatchers.js
@@ -1,14 +1,37 @@
-import shallowHelpers from 'react-shallow-renderer-helpers';
-import objectMatches from 'object-matches';
-import indent from 'indent';
-import inspectReactElement from 'inspect-react-element';
+const isSubset = require('is-subset');
+const shallowHelpers = require('react-shallow-renderer-helpers');
+const indent = require('indent');
+const inspectReactElement = require('inspect-react-element');
+
+//const isSubset = (superset, subset) => {
+//    if (
+//        (typeof superset !== 'object' || superset === null) ||
+//        (typeof subset !== 'object' || subset === null)
+//    ) return false;
+//
+//    return Object.keys(subset).every((key) => {
+//        if (!superset.propertyIsEnumerable(key)) return false;
+//
+//        const subsetItem = subset[key];
+//        const supersetItem = superset[key];
+//        if (
+//            (typeof subsetItem === 'object' && subsetItem !== null) ?
+//                !isSubset(supersetItem, subsetItem) :
+//            supersetItem !== subsetItem
+//        ) return false;
+//
+//        return true;
+//    });
+//};
 
 function findMatching(component, expectedChild) {
     let filteredComponents = shallowHelpers.filterType(component, expectedChild.type);
     let found = false;
 
     filteredComponents.forEach((el) => {
-        found = found || objectMatches(el.props, expectedChild.props);
+        console.log("isSubset -> ", isSubset);
+        console.log("isSubset(el.props, expectedChild.props)-> ", isSubset(el.props, expectedChild.props));
+        found = found || isSubset(el.props, expectedChild.props);
     });
 
     return found;
